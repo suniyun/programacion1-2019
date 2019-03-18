2820bad3d83773756be3d731f605919520eb5a23
Suni <39717985+suniyun@users.noreply.github.com>
2019-03-18 18:10:04 -0300

    Add files via upload

---
+++ Clase1_PSL/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+
+	int a;
+	int b;
+	int resultado;
+
+	printf("\nIngrese 1 numero: ");
+	scanf("%d",&a);
+	system("cls");
+	printf("\nIngrese 2 numero: ");
+	scanf("%d",&b);
+	system("cls");
+	resultado=a-b;
+
+	printf("______________________________________\n");
+	printf("\nEl resultado es: %d \n",resultado);
+	if(resultado>0){
+        printf("\nResultado positivo \n");
+	}
+	if(resultado<0){
+        printf("\nResultado negativo \n");
+	}
+	printf("______________________________________\n\n\n\n");
+
+
+	return 0;
+}
