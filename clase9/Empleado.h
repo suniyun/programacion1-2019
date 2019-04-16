e5e7627515c5bddf0b897e2be0d30ac5eee53744
Suni <39717985+suniyun@users.noreply.github.com>
2019-04-16 12:09:13 -0300

    Add files via upload

---
+++ clase9/Empleado.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#define LIBRE 0
+#define OCUPADO 1
+
+typedef struct
+{
+    int legajo;
+    char nombre[20];
+    char sexo;
+    float sueldoBruto;
+    float sueldoNeto;
+    int estado;
+} eEmpleado;
+
+void mostrarEmpleado(eEmpleado);
+void cargarEmpleado(eEmpleado[], int);
+void mostrarListaEmpleados(eEmpleado[], int);
+
+int buscarLibre(eEmpleado[], int);
+void inicializarEmpleados(eEmpleado[], int);
+void hardcodearDatosEmpleados(eEmpleado[], int);
+void modificarLegajo(eEmpleado lista[], int, int);
+int buscarLegajo(eEmpleado lista[], int, int);
